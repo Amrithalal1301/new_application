34cf7b9cb74bed3f2d8985deb04c1c8f225e08ae
AMRITHA LAL <lalamritha42@gmail.com>
2022-11-14 10:17:40 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,50 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'package:new_application/home.dart';
+
 
 void main(){
    runApp(MaterialApp(
+
      home:MyApp(), ///home screen
    ));
    
   }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomePage()));
+    });
+  }
   @override
   Widget build(BuildContext context) {
   return Scaffold(      ///screen
-     body: Center(
+     body: Container(
+       decoration: const BoxDecoration(
+         image:DecorationImage(
+           fit:BoxFit.fill,
+             image:NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD9iwK_O3T838Um9vX5MHLOcXD3rec_HHmVA&usqp=CAU")),
+       ),
        child: Column(
          mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-           Icon(Icons.add,size: 100,color: Colors.green,),
-           Text("Add File ",
-           style: TextStyle(color: Colors.orangeAccent,fontSize: 30),)
+         children: const [
+           Image(image : AssetImage("assets/images/7247186_christmas_xmas_santa claus_santa_icon.png"),
+           height:200,
+             width:200,
+           ),
+           Text("Merry X'mas ", style: TextStyle(color: Colors.orangeAccent,fontSize: 50),)
          ],
        ),
      )
   );
   }
-                            ///screen creation
-
-
 }
